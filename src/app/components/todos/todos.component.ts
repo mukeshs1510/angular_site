9613c16d6e188b94fd0ad3e557d32e8d4c3172fa
Mukesh Suthar <49620706+mukeshs1510@users.noreply.github.com>
2021-05-07 16:45:21 +0530

    An another commit

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
+})
+export class TodosComponent implements OnInit {
+
+  todos: Todo[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.todos = [{
+      content: 'First todo',
+      isCompleted: false
+    },
+    {
+      content: 'second todo',
+      isCompleted: true
+    }]
+  }
+
+}
